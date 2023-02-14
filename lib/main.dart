48857e193bf4f25088fafdfc3f155b155538eb0b
Leandro <laguierre@gmail.com>
2023-02-14 15:37:00 -0300

    140223

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
 import 'dart:ui';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
@@ -11,11 +10,9 @@ import 'package:permission_handler/permission_handler.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FlutterDownloader.initialize(
-      ignoreSsl: true,
-      debug: true // optional: set false to disable printing logs to console
-      );
+  await FlutterDownloader.initialize(debug: true);
   await Permission.storage.request();
+  FlutterDownloader.registerCallback(DownloadClass.callback);
   runApp(MyApp());
 }
 
@@ -24,13 +21,30 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+class DownloadClass {
+  static void callback(String id, DownloadTaskStatus status, int progress) {
+    print('Download Status: $status');
+    print('Download Progress: $progress');
+  }
+}
+
 class _MyAppState extends State<MyApp> {
   InAppWebViewController? webView;
+  final ReceivePort _port = ReceivePort();
 
   @override
   void initState() {
     super.initState();
     IsolateNameServer.registerPortWithName(
+        _port.sendPort, 'downloader_send_port');
+    _port.listen((dynamic data) {
+      String id = data[0];
+      DownloadTaskStatus status = data[1];
+      int progress = data[2];
+      setState(() {});
+    });
+
+    /* IsolateNameServer.registerPortWithName(
         ReceivePort().sendPort, 'downloader_send_port');
     ReceivePort().listen((dynamic data) {
       String id = data[0];
@@ -49,7 +63,7 @@ class _MyAppState extends State<MyApp> {
       final SendPort? send =
           IsolateNameServer.lookupPortByName('downloader_send_port');
       send?.send([id, status, progress]);
-    });
+    });*/
   }
 
   @override
@@ -63,34 +77,43 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         floatingActionButton: Row(
           children: [
-            Spacer(),
+            const Spacer(),
             FloatingActionButton(
-              child: Icon(Icons.refresh),
+              child: const Icon(Icons.refresh),
               onPressed: () {
                 InAppBrowser.openWithSystemBrowser(
                     url: Uri.parse('http://192.168.4.1/downloadFile.html'));
               },
             ),
-            SizedBox(width: 30),
+            const SizedBox(width: 30),
             FloatingActionButton(
               child: const Icon(Icons.download),
               onPressed: () async {
+                var hasStoragePermission = await Permission.storage.isGranted;
+                if (!hasStoragePermission) {
+                  final status = await Permission.storage.request();
+                  hasStoragePermission = status.isGranted;
+                }
+                var dir = await getExternalStorageDirectory();
+                if (!Directory("${dir!.path}/RAW").existsSync()) {
+                  Directory("${dir!.path}/RAW").createSync(recursive: true);
+                }
                 final taskId = await FlutterDownloader.enqueue(
-                    headers: {
-                      HttpHeaders.connectionHeader: 'keep-alive',
-                      'Content-Disposition':
-                      'Content-Disposition: attachment; filename=prueba.txt'
-                    },
-                    fileName: 'prueba.txt',
-                    url: 'http://192.168.4.1/downloadFile.html',
-                    savedDir: (await getExternalStorageDirectory())!.path,
-                timeout: 300000,
-                showNotification: true,
-                saveInPublicStorage: true,
-
-                // show download progress in status bar (for Android)
-                openFileFromNotification:
-                true, // click on notification to open downloaded file (for Android)
+                  url: 'http://192.168.4.1/downloadFile.html',
+                  //url : "https://firebasestorage.googleapis.com/v0/b/storage-3cff8.appspot.com/o/2020-05-29%2007-18-34.mp4?alt=media&token=841fffde-2b83-430c-87c3-2d2fd658fd41",
+                  headers: {
+                    HttpHeaders.connectionHeader: 'keep-alive',
+                    'Content-Disposition':
+                        'Content-Disposition: attachment; filename=prueba.txt'
+                  },
+                  fileName: 'prueba.txt',
+                  savedDir: dir.path,
+                  //timeout: 300000,
+                  showNotification: true,
+                  //saveInPublicStorage: true,
+                  // show download progress in status bar (for Android)
+                  openFileFromNotification:
+                      false, // click on notification to open downloaded file (for Android)
                 );
               },
             ),
@@ -100,11 +123,11 @@ class _MyAppState extends State<MyApp> {
           title: const Text('InAppWebView Example'),
         ),
         body: Column(children: <Widget>[
-          Expanded(
+          /*Expanded(
               child: InAppWebView(
             key: GlobalKey(),
             initialUrlRequest: URLRequest(
-                url: Uri.parse('http://192.168.4.1/downloadFile.html')),
+                url: Uri.parse('http://192.168.4.1/confDownload.html')),
             // initialHeaders: {},
             initialOptions: InAppWebViewGroupOptions(
               android: AndroidInAppWebViewOptions(
@@ -146,10 +169,10 @@ class _MyAppState extends State<MyApp> {
               print("onDownloadStart $url");
               final taskId = await FlutterDownloader.enqueue(
                 headers: {
-              HttpHeaders.connectionHeader: 'keep-alive',
-              'Content-Disposition':
-                  'Content-Disposition: attachment; filename=prueba.txt'
-            },
+                  HttpHeaders.connectionHeader: 'keep-alive',
+                  'Content-Disposition':
+                      'Content-Disposition: attachment; filename=prueba.txt'
+                },
                 fileName: 'prueba.txt',
                 url: url.toString(),
                 savedDir: (await getExternalStorageDirectory())!.path,
@@ -162,9 +185,144 @@ class _MyAppState extends State<MyApp> {
                     true, // click on notification to open downloaded file (for Android)
               );
             },
-          ))
+          ))*/
         ]),
       ),
     );
   }
 }
+
+/*
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true // optional: set false to disable printing logs to console
+  );
+  FlutterDownloader.registerCallback(DownloadClass.callback);
+  runApp(MyApp());
+}
+
+class DownloadClass{
+  static void callback(String id, DownloadTaskStatus status, int progress){
+    print('Download Status: $status');
+    print('Download Progress: $progress');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int progress = 0;
+
+
+  final ReceivePort _receivePort = ReceivePort();
+
+  static downloadingCallback(id, status, progress) {
+    ///Looking up for a send port
+    SendPort? sendPort = IsolateNameServer.lookupPortByName("downloading");
+
+    ///ssending the data
+    sendPort!.send([id, status, progress]);
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    ///register a send port for the other isolates
+    IsolateNameServer.registerPortWithName(_receivePort.sendPort, "downloading");
+
+
+    ///Listening for the data is comming other isolataes
+    _receivePort.listen((message) {
+      setState(() {
+        progress = message[2];
+      });
+
+      print(progress);
+    });
+
+
+    FlutterDownloader.registerCallback(downloadingCallback);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Text("$progress", style: TextStyle(fontSize: 40),),
+
+            SizedBox(height: 60,),
+
+            TextButton(
+              child: Text("Start Downloading"),
+
+              onPressed: () async {
+                final status = await Permission.storage.request();
+
+                if (status.isGranted) {
+                  final externalDir = await getExternalStorageDirectory();
+
+                  final id = await FlutterDownloader.enqueue(
+                    url:
+                    //"https://todologistica.com/utiles/img2018/formato-contenido-2PB.pdf",
+                    "http://192.168.4.1/downloadFile.html",
+                    savedDir: externalDir!.path,
+                    fileName: "download.pdf",
+                    showNotification: true,
+                    openFileFromNotification: false,
+                  );
+
+
+                } else {
+                  print("Permission deined");
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
